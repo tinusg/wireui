b9c319c0d0dc397f21a62fdb2bdba701826814e6
tinusg <t.guichelaar@gmail.com>
2025-09-29 05:24:21 +0200

    Add autosize property to Textarea component

--- src/View/Components/Textarea.php
+++ src/View/Components/Textarea.php
@@ -4,6 +4,40 @@ namespace WireUi\View\Components;
 
 class Textarea extends Input
 {
+    public bool $autosize;
+
+    public function __construct(
+        bool $autosize = false,
+        // Include all parent Input parameters here
+        ?string $label = null,
+        ?string $hint = null,
+        ?string $cornerHint = null,
+        ?string $icon = null,
+        ?string $rightIcon = null,
+        ?string $prefix = null,
+        ?string $suffix = null,
+        bool $shadowless = false,
+        bool $disabled = false,
+        ?string $name = null,
+        ?string $id = null,
+    ) {
+        $this->autosize = $autosize;
+        
+        parent::__construct(
+            label: $label,
+            hint: $hint,
+            cornerHint: $cornerHint,
+            icon: $icon,
+            rightIcon: $rightIcon,
+            prefix: $prefix,
+            suffix: $suffix,
+            shadowless: $shadowless,
+            disabled: $disabled,
+            name: $name,
+            id: $id,
+        );
+    }
+
     protected function getView(): string
     {
         return 'wireui::components.textarea';
